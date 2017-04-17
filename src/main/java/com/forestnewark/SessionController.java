27268bc689d6765da8233bdf63c219579ea84120
Forest Newark <forest.newark@gmail.com>
2017-04-16 21:01:08 -0400

    added cookies

--- src/main/java/com/forestnewark/SessionController.java
+++ src/main/java/com/forestnewark/SessionController.java
@@ -1,30 +1,64 @@
 package com.forestnewark;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Created by forestnewark on 4/16/17.
- */
+
 @Controller
 public class SessionController {
 
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model,HttpSession session,HttpServletRequest request){
+
+
+        Cookie[] cookies = request.getCookies();
+        session.setAttribute("hasUserName",false);
+        session.setAttribute("hasPassword",false);
+        if (cookies.length > 0){
+            for(int i = 0;i < cookies.length;i++){
+                if(cookies[i].getName().equals("username")){
+                    model.addAttribute("username",cookies[i].getValue());
+                    session.setAttribute("username",cookies[i].getValue());
+                    session.setAttribute("hasUserName",true);
+                }else if(cookies[i].getName().equals("password")){
+                    model.addAttribute("password",cookies[i].getValue());
+                    session.setAttribute("password",cookies[i].getValue());
+                    session.setAttribute("hasPassword",true);
+                }
+            }
+        }
+
         return "index";
     }
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(HttpSession session, HttpServletResponse response){
-        Cookie cookie = new Cookie("crazyCookie","soCrazy!");
-        cookie.setMaxAge(60*60); //1 hour
-        response.addCookie(cookie);
-        System.out.println(session.getId());
+    public String login(HttpSession session, HttpServletResponse response, @RequestParam("inputEmail") String username,@RequestParam("inputPassword") String password){
+
+//        if(session.getAttribute("username").equals("admin") && session.getAttribute("password").equals("admin123")){
+//            model.addAttribute("admin",true);
+//        }else {
+//            model.addAttribute("admin",false);
+//        }
+
+
+        if(!session.getAttribute("hasUserName").equals(true) && !session.getAttribute("hasPassword").equals(true)){
+            Cookie cookieUsername = new Cookie("username", username);
+            Cookie cookiePassword = new Cookie("password", password);
+            cookieUsername.setMaxAge(60*60); //1 hour
+            cookiePassword.setMaxAge(60*60);
+            response.addCookie(cookieUsername);
+            response.addCookie(cookiePassword);
+
+        }
+
         return "welcome";
 
 
